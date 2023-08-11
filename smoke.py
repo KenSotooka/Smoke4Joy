828e70aba23287bca3fbc965ee337df812bbc3cf
popperda <honourscope4.1@gmail.com>
2023-08-11 22:54:45 +0800

    Implemented a likert submit system, can add more

--- smoke.py
+++ smoke.py
@@ -12,6 +12,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
 @app.route("/slide")
 def slideshow(): 
     return render_template("slideshow.html")
