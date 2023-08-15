d6a61467eb649816aba86903ec9a7b69c64859df
popperda <honourscope4.1@gmail.com>
2023-08-16 00:03:01 +0800

    Fixed smoke py code with form, updated it to work
    The function to calculate the score is ready to use, all it needs is the weight and calculation mtrics in the python.

--- smoke.py
+++ smoke.py
@@ -3,8 +3,13 @@ from typing import NewType
 import requests
 from requests.api import post
 
-def weightcalc(asbestos, alcohol, tobacco, UV, radon, Formaldehyde, Meat, genes):
-    return 51 + " high risk of cancer"
+def weightcalc(asbestos, alcohol, tobacco, UV, radon=1, Formaldehyde=1, Meat=1, genes=1):
+    asbestos = int(asbestos)
+    alcohol = int(alcohol)
+    tobacco = int(tobacco)
+    UV = int(UV)
+
+    return (asbestos + alcohol + tobacco + UV)
 
 #just checking name probably the user
 app = Flask(__name__)
@@ -15,10 +20,17 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
-@app.route("/quiz", methods = ["POST"])
+@app.route("/quiz")
 def quiz():
-
     return render_template("quiz.html")
+
+@app.route("/quiz", methods = ["POST"])
+def quizsearch():
+    data = request.form
+    print(data["likert"],data["likert1"],data["likert2"],data["likert3"])
+    data1 = (weightcalc(data["likert"],data["likert1"],data["likert2"],data["likert3"]))
+
+    return render_template("quiz.html", data1 = data1)
 @app.route("/slide")
 def slideshow(): 
     return render_template("slideshow.html")
