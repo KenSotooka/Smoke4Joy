d56817de2873a7819fbfbf2d1001fcbbc62da71c
popperda <honourscope4.1@gmail.com>
2023-08-28 21:56:53 +0800

    Finalised and added try except for the quiz, added one more likert

--- smoke.py
+++ smoke.py
@@ -3,13 +3,14 @@ from typing import NewType
 import requests
 from requests.api import post
 
-def weightcalc(asbestos, alcohol, tobacco, UV, radon=1, Formaldehyde=1, Meat=1, genes=1):
+def weightcalc(asbestos, alcohol, tobacco, UV, radon, Formaldehyde=1, Meat=1, genes=1):
     asbestos = int(asbestos)
     alcohol = int(alcohol)
     tobacco = int(tobacco)
     UV = int(UV)
+    radon = int(radon)
 
-    return (asbestos + alcohol + tobacco + UV)
+    return (asbestos + alcohol + tobacco + UV + 5*radon)
 
 #just checking name probably the user
 app = Flask(__name__)
@@ -26,10 +27,12 @@ def quiz():
 
 @app.route("/quiz", methods = ["POST"])
 def quizsearch():
-    data = request.form
-    print(data["likert"],data["likert1"],data["likert2"],data["likert3"])
-    data1 = (weightcalc(data["likert"],data["likert1"],data["likert2"],data["likert3"]))
-
+    try:
+        data = request.form
+        print(data["likert"],data["likert1"],data["likert2"],data["likert3"])
+        data1 = (weightcalc(data["likert"],data["likert1"],data["likert2"],data["likert3"],data["likert4"]))
+    except KeyError:
+        data1 = "fill in all the boxes!!"
     return render_template("quiz.html", data1 = data1)
 @app.route("/slide")
 def slideshow(): 
