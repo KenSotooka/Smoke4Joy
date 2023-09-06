70c96c254e238b1477517f48fb88c327a25e1f4e
popperda <honourscope4.1@gmail.com>
2023-09-06 22:38:40 +0800

    adding the ticket weights
    recalibrated the weights

--- smoke.py
+++ smoke.py
@@ -3,33 +3,38 @@ from typing import NewType
 import requests
 from requests.api import post
 
-def weightcalc(asbestos, alcohol, tobacco, UV, radon, Formaldehyde=1, Meat=1, genes=1):
+def weightcalc(asbestos, alcohol, tobacco, UV, radon, Formaldehyde=3, Benzene=1, Arsenic=1, Diesel=1, Aflatoxins=1, radiation=1, Benzidine=1):
     asbestos = int(asbestos)
     alcohol = int(alcohol)
     tobacco = int(tobacco)
     UV = int(UV)
     radon = int(radon)
+    Formaldehyde = int(Formaldehyde)
+    Benzene = int(Benzene)
+    Arsenic = int(Arsenic)
+    Diesel = int(Diesel)
+    Aflatoxins = int(Aflatoxins)
+    radiation = int(radiation)
+    Benzidine = int(Benzidine)
 
-    return (2*asbestos + 2*alcohol + 7*tobacco + UV + 5*radon)
+    return (5*asbestos + 3*alcohol + 5*tobacco + 4*UV + 5*radon + 3*Formaldehyde + 4*Benzene + 5*Arsenic+ 4*Diesel + 5*Aflatoxins + 4*radiation + 5*Benzidine)
 
 #just checking name probably the user
 app = Flask(__name__)
 
-#Formaldehyde - 4
 #Asbestos - 5
+#Alcohol - 3
+#Tobacco smoke - 5
+#UV radiation - 4
+#Radon - 5
+#Formaldehyde - 3
 #Benzene - 4
 #Arsenic - 5
-#Tobacco smoke - 5
-#Radon - 5
-#UV radiation - 4
 #Diesel engine exhaust - 4
-#Alcohol - 3
 #Aflatoxins - 5
 #Ionizing radiation - 4
 #Benzidine - 5
-#Vinyl chloride - 4
-#Benzopyrene - 4
-#Cadmium - 5
+
 
 
 
@@ -45,7 +50,7 @@ def quizsearch():
     try:
         data = request.form
         print(data["likert"],data["likert1"],data["likert2"],data["likert3"])
-        data1 = (weightcalc(data["likert"],data["likert1"],data["likert2"],data["likert3"],data["likert4"]))
+        data1 = (weightcalc(data["likert"],data["likert1"],data["likert2"],data["likert3"],data["likert4"],data["likert5"],data["likert6"],data["likert7"],data["likert8"],data["likert9"],data["likert10"],data["likert11"]))
     except KeyError:
         data1 = "fill in all the boxes!!"
     return render_template("quiz.html", data1 = data1)
